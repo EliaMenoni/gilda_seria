52d5a07e6237543f45058e8696adaae9d7d4020c
Elia Menoni <menonielia00@gmail.com>
2021-02-23 17:55:08 +0100

    Reindirizzamento pagina "home"

--- gilda_seria/urls.py
+++ gilda_seria/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView as TP
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+  path('', TP.as_view(template_name='offline.html'), name='home'),
+  path('home', TP.as_view(template_name='offline.html'), name='home'),
+  path('admin/', admin.site.urls),
 ]
